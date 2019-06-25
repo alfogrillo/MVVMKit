2f3745f3db9c6e76661a2d5336227c815c3ecff6
Alfonso Grillo <alfogrillo@gmail.com>
2019-06-25 14:37:57 +0200

    Improve default table view row heights

--- MVVMKit/View Controllers/MVVMTableViewController.swift
+++ MVVMKit/View Controllers/MVVMTableViewController.swift
@@ -134,29 +134,27 @@ open class MVVMTableViewController<Model: TableViewViewModel>: UIViewController,
     // Variable height support
     
     open func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return UITableView.automaticDimension
+        return tableView.rowHeight
     }
     
     open func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        guard sections[section].headerViewModel != nil else { return 0 }
-        return UITableView.automaticDimension
+        return tableView.sectionHeaderHeight
     }
     
     open func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-        guard sections[section].footerViewModel != nil else { return 0 }
-        return UITableView.automaticDimension
+        return tableView.sectionFooterHeight
     }
     
     open func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
-        return UITableView.automaticDimension
+        return tableView.estimatedRowHeight
     }
     
     open func tableView(_ tableView: UITableView, estimatedHeightForHeaderInSection section: Int) -> CGFloat {
-        return UITableView.automaticDimension
+        return tableView.estimatedSectionHeaderHeight
     }
     
     open func tableView(_ tableView: UITableView, estimatedHeightForFooterInSection section: Int) -> CGFloat {
-        return UITableView.automaticDimension
+        return tableView.estimatedSectionFooterHeight
     }
     
     // Section header & footer information
