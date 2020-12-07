3bf2101ac2065366b71fdd2d050b7a7fbe040584
Alfonso Grillo <alfogrillo@gmail.com>
2020-12-07 17:44:44 +0100

    Expand updateLayout api

--- MVVMKit/View Controllers/MVVMDiffableCollectionViewController+CompositionalLayout.swift
+++ MVVMKit/View Controllers/MVVMDiffableCollectionViewController+CompositionalLayout.swift
@@ -26,20 +26,41 @@ THE SOFTWARE.
 
 import UIKit
 
+/**
+ When the `SectionType`s conform to `SectionLayoutConvertible` the class `MVVMDiffableCollectionViewController` provides these methodos, which
+ fully configure the layout of the collection view with an instance of `UICollectionViewCompositionalLayout`.
+ - Note: It is responsibility of the subclass of `MVVMDiffableCollectionViewController` to configure the layout when it is needed.
+ */
 @available(iOS 13.0, *)
 public extension MVVMDiffableCollectionViewController where ViewModelType.SectionType: SectionLayoutConvertible {
-    /**
-     When the `SectionType`s conform to `SectionLayoutConvertible` the class `MVVMDiffableCollectionViewController` provides this method, which
-     fully configures the layout of the collection view with an instance of `UICollectionViewCompositionalLayout`.
-     - Note: It is responsibility of the subclass of `MVVMDiffableCollectionViewController` to configure the layout when it is needed.
-     */
     @discardableResult
     func updateLayout(animated: Bool = true) -> UICollectionViewCompositionalLayout {
-        let layout: UICollectionViewCompositionalLayout = .init { [weak self] (index, environment) -> NSCollectionLayoutSection? in
-            self?.dataSource.snapshot().sectionIdentifiers[index].sectionLayout(with: environment)
-        }
+        updateLayout(type: UICollectionViewCompositionalLayout.self, animated: animated)
+    }
+
+    @discardableResult
+    func updateLayout(animated: Bool = true, configuration: UICollectionViewCompositionalLayoutConfiguration) -> UICollectionViewCompositionalLayout {
+        updateLayout(type: UICollectionViewCompositionalLayout.self, animated: animated, configuration: configuration)
+    }
+
+    @discardableResult
+    func updateLayout<Layout: UICollectionViewCompositionalLayout>(type: Layout.Type, animated: Bool = true) -> Layout {
+        let layout = Layout.init(sectionProvider: sectionProvider)
         collectionView.setCollectionViewLayout(layout, animated: animated)
         return layout
+    }
+
+    @discardableResult
+    func updateLayout<Layout: UICollectionViewCompositionalLayout>(type: Layout.Type, animated: Bool = true, configuration: UICollectionViewCompositionalLayoutConfiguration) -> Layout {
+        let layout = Layout.init(sectionProvider: sectionProvider, configuration: configuration)
+        collectionView.setCollectionViewLayout(layout, animated: animated)
+        return layout
+    }
+
+    private var sectionProvider: UICollectionViewCompositionalLayoutSectionProvider {
+        return { [weak self] (index, environment) -> NSCollectionLayoutSection? in
+            self?.dataSource.snapshot().sectionIdentifiers[index].sectionLayout(with: environment)
+        }
     }
 }
 
